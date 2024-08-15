951654ddcf1d3da9c3a8fd5d590db720a9cdd603
Muhammad Anas Darwish <muhammadanasdarwish1@gmail.com>
2024-08-15 19:55:14 +0300

    change auth controller response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\RegisterUserRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
 use App\Services\AuthService;
+use App\Traits\ApiResponses;
 
 class AuthController extends Controller
 {
+    use ApiResponses;
+
     public function __construct(protected AuthService $authService)
     {
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
@@ -21,17 +25,17 @@ class AuthController extends Controller
         $token = Auth::attempt($credentials);
 
         if (!$token) {
-            return response()->unauthorized();
+            return $this->errorResponse('User unauthorized', 401);
         }
 
         $user = Auth::user();
-        return response()->ok([
-            'user' => $user,
+        return $this->successResponse([
+            'user' => new UserResource($user),
             'authorization' => [
                 'token' => $token,
                 'type' => 'bearer',
             ],
-        ]);
+        ], 'Login Successfully', 200);
     }
 
     public function register(RegisterUserRequest $request)
@@ -41,33 +45,30 @@ class AuthController extends Controller
         $user = $this->authService->createUser($data);
         $token = Auth::login($user);
 
-        return response()->created(
-            [
-                'user' => $user,
-                'authorization' => [
-                    'token' => $token,
-                    'type' => 'bearer',
-                ],
+        return $this->successResponse([
+            'user' => new UserResource($user),
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
             ],
-            'User created successfully',
-        );
+        ], 'Register successfully', 201);
     }
 
     public function logout()
     {
         Auth::logout();
 
-        return response()->ok([], 'Successfully logged out');
+        return $this->successResponse([], 'Successfully logged out');
     }
 
     public function refresh()
     {
-        return response()->ok([
-            'user' => Auth::user(),
+        return $this->successResponse([
+            'user' => new UserResource(Auth::user()),
             'authorization' => [
                 'token' => Auth::refresh(),
                 'type' => 'bearer',
             ],
-        ]);
+        ], 'Token refreshed successfully', 200);
     }
 }
